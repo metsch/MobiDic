9b91a04c1e169966d313fca4427edabf4caeedae
Michael Etschbacher <metschbacher.mmt-b2016@fh-salzburg.ac.at>
2018-11-08 15:28:06 +0100

    added basic security

---
+++ src/main/java/com/mobidic/authentication/WebSecurityConfig.java
@@ -0,0 +1,33 @@
+package com.mobidic.authentication;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/").permitAll().anyRequest().authenticated().and().formLogin().permitAll()
+                .and().logout().permitAll();
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder().username("admin").password("admin").roles("USER").build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+}
